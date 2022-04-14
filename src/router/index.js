898475443d21a58b70d8ed0fc71d81f82b060a55
Trọng Nhân <58482915+trongnhan190901@users.noreply.github.com>
2022-04-15 01:47:51 +0700

    started

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createWebHistory, createRouter } from "vue-router";
+import Home from '@/views/Home.vue';
+import Manga from '@/views/Manga.vue'
+import Character from '@/views/Character.vue'
+import Author from '@/views/Author.vue'
+import About from '@/views/About.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
+const routes= [
+    {
+        path: '/', 
+        name: 'Home', 
+        component:Home
+    },
+    {
+        path: '/manga', 
+        name: 'Manga', 
+        component:Manga
+    },
+    {
+        path: '/character', 
+        name: 'Character', 
+        component:Character
+    },
+    {
+        path: '/author', 
+        name: 'Author', 
+        component:Author
+    },
+    {
+        path: '/about', 
+        name: 'About', 
+        component:About
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
-
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  })
+  
 export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <video autoplay loop muted class="fixed left-0 top-0 min-h-full min-w-full mx-auto opacity-40 z-0 -m-4">
+    <source src="../../public/media/video/Intro.mp4" type="video/mp4" >
+  </video>
+
+  <div class="text-white w-[635px] fixed left-32 bottom-80">
+    <div class="text-5xl text-center bottom-3">
+     KIMETSU NO YAIBA
+    </div>
+    <div class="text-2xl text-justify pt-4">
+     Vào thời Taisho, có một cậu bé bán than với tấm lòng nhân hậu tên là Tanjiro. Những ngày yên bình đã chẳng còn khi Quỷ đến tàn sát cả gia đình cậu, chỉ duy nhất người em gái Nezuko còn sống nhưng lại bị biến thành Quỷ. Mang trong mình quyết tâm chữa cho em gái, Tanjiro cùng Nezuko bắt đầu cuộc hành trình tìm kiếm tung tích con quỷ đã ra tay với gia đình mình !!
+    </div>
+    <div class="flex items-center justify-center top-4 flex-wrap pt-4">
+      <router-link :to="{}" class="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-pink-500 to-red-400 hover:from-red-600 hover:to-red-500 active:from-red-600 active:to-red-800 focus-visible:ring ring-pink-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2 mx-2">
+        Xem Anime
+      </router-link>
+      <router-link :to="{name: 'Manga'}" class="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-pink-500 to-red-400 hover:from-red-600 hover:to-red-500 active:from-red-600 active:to-red-800 focus-visible:ring ring-pink-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2 mx-2">
+        Xem Manga
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeCom'
+}
+</script>
